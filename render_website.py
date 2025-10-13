540cf3213db9efdd2fc176429e94d547e1309f48
abemka1234 <alesparfenov@gmail.com>
2025-10-13 17:38:14 +0300

    Added paginator

--- render_website.py
+++ render_website.py
@@ -15,15 +15,17 @@ with open("meta_data.json", "r",encoding="UTF8") as my_file:
     books = json.load(my_file)
 
 books_pages = list(more_itertools.chunked(books, 15))
-os.makedirs("pages")
+os.makedirs("pages",exist_ok=True)
 
 for i, books_part in enumerate(books_pages, 1):
     rendered_page = template.render(
         books = books_part,
+        pages_count = len(books_pages),
+        current_page = i,
     )
     with open('pages/index{}.html'.format(i), 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
 server = Server()
 server.watch('*.html', shell('make html', cwd='docs'))
-server.serve(root='')
+server.serve(root='',default_filename="pages/index1.html")
