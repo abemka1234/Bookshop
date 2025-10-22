a487c1b2dbab330f07f04defe9a7be75c1187eb9
abemka1234 <alesparfenov@gmail.com>
2025-10-22 17:54:17 +0300

    fixed some bugs

--- render_website.py
+++ render_website.py
@@ -1,33 +1,38 @@
 import json
+import os
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server, shell
 import more_itertools
-import os
-def main():
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
+from dotenv import load_dotenv 
 
-    template = env.get_template('template.html')
-    with open("meta_data.json", "r",encoding="UTF8") as my_file:
+def on_reload():
+    env = Environment(
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape(["html", "xml"])
+    )
+    load_dotenv()
+    template = env.get_template("template.html")
+    with open(os.getenv("FILENAME_BOOKLIST", "meta_data.json"), "r",encoding="UTF8") as my_file:
         books = json.load(my_file)
 
     books_pages = list(more_itertools.chunked(books, 15))
     os.makedirs("pages",exist_ok=True)
 
-    for i, books_part in enumerate(books_pages, 1):
+    for books_number, books_part in enumerate(books_pages, 1):
         rendered_page = template.render(
             books = books_part,
             pages_count = len(books_pages),
-            current_page = i,
+            current_page = books_number,
         )
-        with open('pages/index{}.html'.format(i), 'w', encoding="utf8") as file:
+        with open("pages/index{}.html".format(books_number), "w", encoding="utf8") as file:
             file.write(rendered_page)
 
+def main():
+    on_reload()
     server = Server()
-    server.watch('*.html', shell('make html', cwd='docs'))
-    server.serve(root='',default_filename="pages/index1.html")
+    server.watch("template.html", on_reload)
+    server.serve(root="",default_filename="pages/index1.html")
 
 
 if __name__ == "__main__":
